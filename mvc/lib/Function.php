be5a8a7bc52d89197f9b0ede9ba1e400e235010f
duubs <2603559572@qq.com>
2018-03-13 11:42:07 +0800

    完善 命名空间

--- mvc/lib/Function.php
+++ mvc/lib/Function.php
@@ -6,7 +6,8 @@ function M($table_name)
 	static $table_obj = []; // 对象集合
 	//判断集合内是否有 本章表,没有添加进去
 	if (!isset($table_obj[$table_name])) {
-		$table_obj[$table_name] = new $table_name;
+		$table_names = '\\model\\'.$table_name;
+		$table_obj[$table_name] = new $table_names;
 	}
 	return $table_obj[$table_name]; 
 }
@@ -14,18 +15,7 @@ function M($table_name)
 //自动加载文件
 function __autoload($class)
 {
-	$arra = [
-		ROOT_DIR.'/lib/'.$class.'.php',
-		ROOT_DIR.'/controller/'.$class.'.php',
-		ROOT_DIR.'/view/'.$class.'.php',
-		ROOT_DIR.'/model/'.$class.'.php',
-	];
-	foreach ($arra as $k => $v) {
-		if (file_exists($v)) {
-			include $v;
-		}
-	}
-
+	include_once ROOT_DIR.'/'.str_replace('\\', '/', $class).'.php';
 }
 
 
